fb9d98d2f8babe14c0c453277930e37b37996852
Ivan Chien <qyc027@gmail.com>
2022-11-15 15:36:39 +0800

    Update wezterm configs

--- wezterm.lua
+++ wezterm.lua
@@ -1,6 +1,8 @@
 local w = require 'wezterm'
 return {
-    font = w.font('OperatorMono Nerd Font', { weight = 'Book' }),
-    font_size = 16.0,
+    font = w.font('MonoLisa', { weight = 'Regular' }),
+    font_size = 14.0,
     color_scheme = "Hybrid (terminal.sexy)",
+    scrollback_lines = 50000,
+    audible_bell = "Disabled",
 }
