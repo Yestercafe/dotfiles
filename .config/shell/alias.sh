a8f202c34466e19003aeb14ba1c7c531eb0ad702
Ivan Chien <qyc027@outlook.com>
2025-09-11 14:54:15 +0800

    feat: add more shell functions and aliases

--- .config/shell/alias.sh
+++ .config/shell/alias.sh
@@ -7,4 +7,9 @@ alias gb="g branch"
 alias gss="g stash"
 alias glog="g log --graph --oneline"
 
-alias mcd="mkdircd"
+alias mcd="ic_mkdircd"
+alias cdtmp="ic_make_tmp_dir_and_cd"
+alias ic-backup="ic_backup_file_or_dir"
+alias ic-persist="ic_persist_file_or_dir"
+alias ic-yk="ic_yank_to_clipboard"
+
--- .config/shell/function.sh
+++ .config/shell/function.sh
@@ -1,18 +1,69 @@
-rr() {
-    urandom_str=$(head -c 4 /dev/urandom | xxd -p | tr -d '\n')
+ic_get_random_string() {
     if [[ "$#" == "0" ]]; then
-        mkdir -p "$urandom_str"
-        echo "Create a dir named \`$urandom_str\`"
+        head -c 4 /dev/urandom | xxd -p | tr -d '\n'
     else
-        pattern=$1
-        file_name="${pattern/\{\}/$urandom_str}"
-        touch "$file_name"
-        echo "Touch a file named \`$file_name\`"
+        head -c $1 /dev/urandom | xxd -p | tr -d '\n'
     fi
 }
 
-mkdircd() {
+ic_mkdircd() {
     mkdir -p $1 && cd $1
 }
 
+ic_make_tmp_dir() {
+    mkdir -vp /tmp/$(ic_get_random_string)
+}
+
+ic_make_tmp_file() {
+    local extension="$1"
+    local dst="$2"
+    if [[ -z $extension ]]; then
+        echo "Usage: $0 extension [dst]"
+        return -1
+    fi
+    if [[ -z $dst ]]; then
+        dst_file="/tmp/$(ic_get_random_string).${extension}"
+    else
+        dst_file="${dst}/$(ic_get_random_string).${extension}"
+    fi
+    touch "$dst_file"
+    local ret=$?
+    echo -n "$dst_file"
+    return $ret
+}
+
+ic_make_tmp_dir_and_cd() {
+    local tmp_dir=$(ic_make_tmp_dir)
+    cd ${tmp_dir}
+}
+
+ic_persist_file_or_dir() {
+    local dst="$HOME/.persistence"
+    mkdir -p "${dst}"
+    local this_dir="$PWD"
+    pushd "${dst}"
+    cp -rv "${this_dir}" "${dst}"
+    popd
+}
+
+ic_backup_file_or_dir() {
+    if [[ "$#" == 0 ]]; then
+        echo "Usage: $0 [path]"
+        return 0
+    else
+        if [[ -e "$1" ]]; then
+            cp -rv "$1" "$1~"
+        else
+            echo "$1 not exists"
+        fi
+        return $?
+    fi
+}
+
+ic_yank_to_clipboard() {
+    local os="$(uname)"
+    if [[ "$os" == "Darwin" ]]; then
+        pbcopy
+    fi
+}
 
